50d8098d3ad896d675ddef1c992e2a23ad13a39b
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2024-04-05 17:37:48 -0600

    Inicio de proyecto - Bases de JS/TS

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import './style.css';
+import typescriptLogo from './typescript.svg';
+import viteLogo from '/vite.svg';
+
+import '../bases/01-const-let';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    
+  </div>
+`
+
+
+
