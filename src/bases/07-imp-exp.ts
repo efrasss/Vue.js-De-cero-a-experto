a0d734c7abd166790c016f6cb76f12990e31f59e
efrasss <efrainska5@gmail.com>
2024-04-08 20:26:34 -0600

    Importaciones y Exportaciones

---
+++ src/bases/07-imp-exp.ts
@@ -0,0 +1,14 @@
+import heroes, { owners } from "../data/heroes";
+
+
+export const getHeroById = ( id: number ) => {
+    return heroes.find( hero => hero.id === id);
+}
+
+export const getHeroesByOwner = (owner: string  ) => {
+    return heroes.filter( hero => hero.owner === owner )
+}
+
+console.log( getHeroesByOwner('DC') );
+
+
